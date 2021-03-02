073effa19f13ccc48806ecf8777c4741cb13d9e8
angelinn <angelin.nedelchev@outlook.com>
2021-03-02 13:39:51 +0200

    Add main class.

---
+++ src/main/java/DiscordBot/DiscordBot/Main.java
@@ -0,0 +1,12 @@
+package DiscordBot.DiscordBot;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+import javax.security.auth.login.LoginException;
+
+public class Main {
+	public static void main(String[] args) throws LoginException, InterruptedException, IOException, SQLException {
+		Bot.main(args);
+	}
+}
